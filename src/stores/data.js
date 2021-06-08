975c9c15f1789c97a111c738ea8939f40625f531
higsch <matthias@higsch.com>
2021-06-08 13:57:16 +0200

    More filter logic

--- src/stores/data.js
+++ src/stores/data.js
@@ -1,3 +1,45 @@
-import { writable } from 'svelte/store';
+import { writable, derived } from 'svelte/store';
+
+import {
+  statusFilter,
+  technologyFilter,
+  infrastructureFilter,
+  accessFilter,
+  corporatePartnershipFilter,
+  crossborderPartnershipsFilter
+} from './filter';
+import { hasOverlap } from '../utils/logic';
 
 export const rawData = writable([]);
+
+export const data = derived([
+  rawData,
+  statusFilter,
+  technologyFilter,
+  infrastructureFilter,
+  accessFilter,
+  corporatePartnershipFilter,
+  crossborderPartnershipsFilter
+], ([
+  $rawData,
+  $statusFilter,
+  $technologyFilter,
+  $infrastructureFilter,
+  $accessFilter,
+  $corporatePartnershipFilter,
+  $crossborderPartnershipsFilter
+]) => {
+  return $rawData.map(d => {
+    return {
+      ...d,
+      show: hasOverlap(d.current_status, $statusFilter) &&
+            hasOverlap(d.technology, $technologyFilter) &&
+            hasOverlap(d.infrastructure, $infrastructureFilter) &&
+            hasOverlap(d.access, $accessFilter) &&
+            hasOverlap(d.corporate_partnership, $corporatePartnershipFilter) &&
+            hasOverlap(d.crossborder_partnerships, $crossborderPartnershipsFilter)
+    };
+  });
+});
+
+export const mapData = writable([]);
---
+++ src/utils/logic.js
@@ -0,0 +1,3 @@
+export const hasOverlap = (arr, filter) => {
+  return filter.filter((d) => d.selected).map((d) => d.id).some((item) => arr.includes(item));
+};
